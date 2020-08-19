25beccad2a2b97a788678601237cd48c17763e33
xevi <xevi@xevi>
2020-08-19 11:49:55 +0200

    Maven Project
    Created with https://start.spring.io/

---
+++ SpringBootRest/src/main/java/com/xevi/system/SpringBootRest/SpringBootRestApplication.java
@@ -0,0 +1,13 @@
+package com.xevi.system.SpringBootRest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRestApplication.class, args);
+	}
+
+}
---
+++ SpringBootRest/src/test/java/com/xevi/system/SpringBootRest/SpringBootRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xevi.system.SpringBootRest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
